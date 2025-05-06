0a39acd9afa21862ba1cc3c0737ee9f1023245b0
gneiru <noelrohi59@gmail.com>
2025-05-06 11:55:56 +0800

    chore: update APP_DOMAIN to remove protocol in constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const APP_DOMAIN = "https://usetestify.xyz";
+export const APP_DOMAIN = "usetestify.xyz";
 export const APP_URL = "https://usetestify.xyz";
 export const GITHUB_REPO_URL = "https://github.com/noelrohi/testify";
